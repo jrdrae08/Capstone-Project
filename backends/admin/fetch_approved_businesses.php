0850a89ee9e51cd7a5d9a0523a1a22db7f5ff115
Dano <jordanmarabe080802@gmail.com>
2024-10-06 16:28:43 +0800

    Refactor fetch_approved_businesses.php to order results by creation date

--- backends/admin/fetch_approved_businesses.php
+++ backends/admin/fetch_approved_businesses.php
@@ -5,17 +5,18 @@ function getApprovedBusinesses($pdo)
 {
   try {
     $stmt = $pdo->prepare("SELECT b.ApplicationID, DATE_FORMAT(b.CreatedAt, '%Y-%m-%d') AS 'Date Registered', 
-                                  t.TypeName AS BusinessType, i.BusinessName, i.BusinessAddress, 
-                                  i.BusinessEmail AS BusinessEmail, i.BusinessContactNumber AS BusinessContactNumber,
-                                  b.RegistrantFirstName, b.RegistrantMiddleName, b.RegistrantLastName, 
-                                  b.ContactNumber AS RegistrantContact, b.Email AS RegistrantEmail, 
-                                  b.BusinessPermitImage,
-                                  a.AccountID, a.BusinessStatus
-                            FROM businessapplicationform b
-                            JOIN businessinformationform i ON b.ApplicationID = i.ApplicationID
-                            JOIN businesstype t ON i.BusinessTypeID = t.BusinessTypeID
-                            JOIN account a ON b.ApplicationID = a.ApplicationID
-                            WHERE b.Status = 'Approved' AND a.BusinessArchive = 0");
+                t.TypeName AS BusinessType, i.BusinessName, i.BusinessAddress, 
+                i.BusinessEmail AS BusinessEmail, i.BusinessContactNumber AS BusinessContactNumber,
+                b.RegistrantFirstName, b.RegistrantMiddleName, b.RegistrantLastName, 
+                b.ContactNumber AS RegistrantContact, b.Email AS RegistrantEmail, 
+                b.BusinessPermitImage,
+                a.AccountID, a.BusinessStatus
+            FROM businessapplicationform b
+            JOIN businessinformationform i ON b.ApplicationID = i.ApplicationID
+            JOIN businesstype t ON i.BusinessTypeID = t.BusinessTypeID
+            JOIN account a ON b.ApplicationID = a.ApplicationID
+            WHERE b.Status = 'Approved' AND a.BusinessArchive = 0
+            ORDER BY b.CreatedAt DESC");
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
   } catch (PDOException $e) {
